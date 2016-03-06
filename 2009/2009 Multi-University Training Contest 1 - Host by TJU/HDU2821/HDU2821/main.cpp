2cf46fb499a2b3e8a7ab9f664acc0a3d31fd3fd2
beMaster <bemaster@181.211.84.110.broad.fz.fj.dynamic.163data.com.cn>
2016-03-06 17:10:49 +0800

    dfs

---
+++ 2009/2009 Multi-University Training Contest 1 - Host by TJU/HDU2821/HDU2821/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HDU2821
+//
+//  Created by beMaster on 16/3/6.
+//  Copyright © 2016年 beMaster. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
